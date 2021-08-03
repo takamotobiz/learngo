a315e4f915f35089925496f90e3300d9f353fd0d
takamotobiz <keiji.takamoto@takamoto.biz>
2021-08-03 16:22:57 +0900

    osimum対応

--- main.go
+++ main.go
@@ -3,170 +3,143 @@ package main
 import (
 	"context"
 	"fmt"
-	"math"
+	"log"
 	"os"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/paulmach/osm"
 	"github.com/paulmach/osm/osmpbf"
 )
 
-// ExampleStats demonstrates how to read a full file and gather some stats.
-// This is similar to `osmconvert --out-statistics`
 func main() {
-	f, err := os.Open("/Users/takamotokeiji/data/osm.pbf/planet-latest.osm.pbf")
+
+	start := time.Now()
+
+	f, err := os.Open("./japan-low.osm.pbf")
+	//f, err := os.Open("./shikoku-low.osm.pbf")
+	// f, err := os.Open("../../data/japan-low.osm.pbf")
+	// f, err := os.Open("../../data/planet-low.osm.pbf")
 	if err != nil {
 		fmt.Printf("could not open file: %v", err)
 		os.Exit(1)
 	}
 	defer f.Close()
 
+	// ファイルを書き込み用にオープン (mode=0666)
+	file, err := os.Create("./output.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// FeatureCollectionレコード（ヘッダー的なもの）を出力
+	file.WriteString("{\"type\":\"FeatureCollection\",\"features\":[\n")
+
 	nodes, ways, relations := 0, 0, 0
-	stats := newElementStats()
+	snodes, sways, srelations := 0, 0, 0
+	cpu := runtime.NumCPU()
 
-	minLat, maxLat := math.MaxFloat64, -math.MaxFloat64
-	minLon, maxLon := math.MaxFloat64, -math.MaxFloat64
-
-	minTS, maxTS := time.Date(2100, 1, 1, 0, 0, 0, 0, time.UTC), time.Time{}
-
-	var (
-		maxNodeRefs   int
-		maxNodeRefsID osm.WayID
-	)
-
-	var (
-		maxRelRefs   int
-		maxRelRefsID osm.RelationID
-	)
-
-	scanner := osmpbf.New(context.Background(), f, 3)
+	scanner := osmpbf.New(context.Background(), f, cpu)
 	defer scanner.Close()
 
+	var endl bool
 	for scanner.Scan() {
-		var ts time.Time
 
 		switch e := scanner.Object().(type) {
 		case *osm.Node:
+			if e.Tags.Find("amenity") == "school" {
+				snodes++
+				// 最後のレコード出力時にはカンマを出力しない
+				if endl {
+					file.WriteString(",\n")
+				} else {
+					endl = true
+				}
+				// 要素情報の出力
+				file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",")
+				file.WriteString(fmt.Sprintf("\"coordinates\":[%.7f,%.7f]}", e.Lon, e.Lat))
+				// 属性文字のエスケープ関連文字の訂正
+				if strings.Contains(e.Tags.Find("name"), "\\") {
+					file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\\", "", -1)))
+				} else if strings.Contains(e.Tags.Find("name"), "\n") {
+					file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\n", "", -1)))
+				} else if strings.Contains(e.Tags.Find("name"), "\"") {
+					file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\"", "　", -1)))
+				} else {
+					file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", e.Tags.Find("name")))
+				}
+			}
 			nodes++
-			ts = e.Timestamp
-			stats.Add(e.ElementID(), e.Tags)
-
-			if e.Lat > maxLat {
-				maxLat = e.Lat
-			}
-
-			if e.Lat < minLat {
-				minLat = e.Lat
-			}
-
-			if e.Lon > maxLon {
-				maxLon = e.Lon
-			}
-
-			if e.Lon < minLon {
-				minLon = e.Lon
-			}
 		case *osm.Way:
+			if e.Tags.Find("amenity") == "school" {
+				sways++
+				// 最後のレコード出力時にはカンマを出力しない
+				if endl {
+					file.WriteString(",\n")
+				} else {
+					endl = true
+				}
+				// 要素情報の出力
+				if e.Polygon() {
+					file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",")
+					file.WriteString("\"coordinates\":[[")
+				} else {
+					file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"LineString\",")
+					file.WriteString("\"coordinates\":[")
+				}
+				for i, v := range e.Nodes {
+					if i > 0 {
+						file.WriteString(",")
+					}
+					file.WriteString(fmt.Sprintf("[%.7f,%.7f]", v.Lon, v.Lat))
+				}
+				if e.Polygon() {
+					file.WriteString("]]}")
+				} else {
+					file.WriteString("]}")
+				}
+
+				// file.WriteString(fmt.Sprintf("\"coordinates\":[%.7f,%.7f]}", e.Lon, e.Lat))
+				// 属性文字のエスケープ関連文字の訂正
+				if strings.Contains(e.Tags.Find("name"), "\\") {
+					file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\\", "", -1)))
+				} else if strings.Contains(e.Tags.Find("name"), "\n") {
+					file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\n", "", -1)))
+				} else if strings.Contains(e.Tags.Find("name"), "\"") {
+					file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\"", "　", -1)))
+				} else {
+					file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", e.Tags.Find("name")))
+				}
+			}
 			ways++
-			ts = e.Timestamp
-			stats.Add(e.ElementID(), e.Tags)
-
-			if l := len(e.Nodes); l > maxNodeRefs {
-				maxNodeRefs = l
-				maxNodeRefsID = e.ID
-			}
 		case *osm.Relation:
+			srelations++
 			relations++
-			ts = e.Timestamp
-			stats.Add(e.ElementID(), e.Tags)
-
-			if l := len(e.Members); l > maxRelRefs {
-				maxRelRefs = l
-				maxRelRefsID = e.ID
-			}
 		}
 
-		if ts.After(maxTS) {
-			maxTS = ts
-		}
-
-		if ts.Before(minTS) {
-			minTS = ts
-		}
 	}
+	// FeatureCollection終端を出力
+	file.WriteString("]}\n")
 
 	if err := scanner.Err(); err != nil {
 		fmt.Printf("scanner returned error: %v", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("timestamp min:", minTS.Format(time.RFC3339))
-	fmt.Println("timestamp max:", maxTS.Format(time.RFC3339))
-	fmt.Printf("lon min: %0.7f\n", minLon)
-	fmt.Printf("lon max: %0.7f\n", maxLon)
-	fmt.Printf("lat min: %0.7f\n", minLat)
-	fmt.Printf("lat max: %0.7f\n", maxLat)
+	end := time.Now()
+
+	fmt.Println("Start:", start)
+	fmt.Println("End  :", end)
+	fmt.Println("Elapsed:", end.Sub(start))
+
 	fmt.Println("nodes:", nodes)
 	fmt.Println("ways:", ways)
 	fmt.Println("relations:", relations)
-	fmt.Println("version max:", stats.MaxVersion)
-	fmt.Println("node id min:", stats.Ranges[osm.TypeNode].Min)
-	fmt.Println("node id max:", stats.Ranges[osm.TypeNode].Max)
-	fmt.Println("way id min:", stats.Ranges[osm.TypeWay].Min)
-	fmt.Println("way id max:", stats.Ranges[osm.TypeWay].Max)
-	fmt.Println("relation id min:", stats.Ranges[osm.TypeRelation].Min)
-	fmt.Println("relation id max:", stats.Ranges[osm.TypeRelation].Max)
-	fmt.Println("keyval pairs max:", stats.MaxTags)
-	fmt.Println("keyval pairs max object:", stats.MaxTagsID.Type(), stats.MaxTagsID.Ref())
-	fmt.Println("noderefs max:", maxNodeRefs)
-	fmt.Println("noderefs max object: way", maxNodeRefsID)
-	fmt.Println("relrefs max:", maxRelRefs)
-	fmt.Println("relrefs max object: relation", maxRelRefsID)
 
-}
+	fmt.Println("snodes:", snodes)
+	fmt.Println("sways:", sways)
+	fmt.Println("srelations:", srelations)
 
-// Stats is a shared bit of code to accumulate stats from the element ids.
-type elementStats struct {
-	Ranges     map[osm.Type]*idRange
-	MaxVersion int
-
-	MaxTags   int
-	MaxTagsID osm.ElementID
-}
-
-type idRange struct {
-	Min, Max int64
-}
-
-func newElementStats() *elementStats {
-	return &elementStats{
-		Ranges: map[osm.Type]*idRange{
-			osm.TypeNode:     {Min: math.MaxInt64},
-			osm.TypeWay:      {Min: math.MaxInt64},
-			osm.TypeRelation: {Min: math.MaxInt64},
-		},
-	}
-}
-
-func (s *elementStats) Add(id osm.ElementID, tags osm.Tags) {
-	s.Ranges[id.Type()].Add(id.Ref())
-
-	if v := id.Version(); v > s.MaxVersion {
-		s.MaxVersion = v
-	}
-
-	if l := len(tags); l > s.MaxTags {
-		s.MaxTags = l
-		s.MaxTagsID = id
-	}
-}
-
-func (r *idRange) Add(ref int64) {
-	if ref > r.Max {
-		r.Max = ref
-	}
-
-	if ref < r.Min {
-		r.Min = ref
-	}
 }
