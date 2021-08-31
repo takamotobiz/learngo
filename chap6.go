de0129f5e5bc9efee46d5dc9d483c766cb3bf0a9
takamotobiz <keiji.takamoto@takamoto.biz>
2021-08-31 19:52:21 +0900

    main

--- chap6.go
+++ chap6.go
@@ -14,9 +14,10 @@ import (
 
 // define file
 // const pbfname string = "/data/osm.pbf/shikoku-low.osm.pbf"
-// const pbfname string = "/data/osm.pbf/japan-low.osm.pbf"
+const pbfname string = "/data/osm.pbf/japan-low.osm.pbf"
+
 // const pbfname string = "/data/osm.pbf/planet-low.osm.pbf"
-const pbfname string = "/data/osm.pbf/hokkaido-low.osm.pbf"
+// const pbfname string = "/data/osm.pbf/hokkaido-low.osm.pbf"
 const outfname string = "./output.json"
 
 // define Tag
@@ -25,9 +26,12 @@ const tagval string = "school"
 
 func main() {
 
+	// get start time
 	start := time.Now()
 
-	// open osm.pbf file
+	// ====================================
+	// 1-1.open osm.pbf file and create scanner
+	// ====================================
 	home, _ := os.UserHomeDir()
 	f, err := os.Open(home + pbfname)
 	if err != nil {
@@ -36,28 +40,20 @@ func main() {
 	}
 	defer f.Close()
 
-	// create GeoJSON file
-	file, err := os.Create(outfname)
-	if err != nil {
-		fmt.Printf("could not create file: %v", err)
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	// get number of cpu and set procs
+	// get number of cpu and create scanner
 	cpu := runtime.NumCPU()
 	scanner := osmpbf.New(context.Background(), f, cpu)
 
-	// Create Node list include Relations
+	// ====================================
+	// 1-1.create Node list include Relations(mrway)
+	// ====================================
 	scanner.SkipNodes = true
 	scanner.SkipWays = true
-	rrelations := 0
 	mrway := map[int]string{}
 	for scanner.Scan() {
 		switch re := scanner.Object().(type) {
 		case *osm.Relation:
 			if re.Tags.Find(tagname) == tagval {
-				rrelations++
 				for _, v := range re.Members {
 					mrway[int(v.Ref)] = string(v.Type) + "/" + v.Role + "/"
 				}
@@ -66,111 +62,75 @@ func main() {
 	}
 	scanner.Close()
 
-	// Recreate scanner( and rewind file )
+	// ====================================
+	// 1-2.recreate scanner and add coordinate to mrway
+	// ====================================
 	f.Seek(0, 0)
 	scanner = osmpbf.New(context.Background(), f, cpu)
-
-	// FeatureCollectionレコード（ヘッダー的なもの）を出力
-	file.WriteString("{\"type\":\"FeatureCollection\",\"features\":[\n")
-
-	nodes, ways, relations := 0, 0, 0
-	snodes, sways, srelations := 0, 0, 0
-	// var endl bool
-
-	// for debug
 	scanner.SkipNodes = true
-
+	scanner.SkipRelations = true
 	for scanner.Scan() {
-
-		f := scanner.Object().ObjectID().Type()
-		fmt.Println("Node!\n", f)
-
-		switch e := scanner.Object().(type) {
-		case *osm.Node:
-			// if e.Tags.Find(tagname) == tagval {
-			// 	snodes++
-			// 	// 最後のレコード出力時にはカンマを出力しない
-			// 	if endl {
-			// 		file.WriteString(",\n")
-			// 	} else {
-			// 		endl = true
-			// 	}
-			// 	// 要素情報の出力
-			// 	file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",")
-			// 	file.WriteString(fmt.Sprintf("\"coordinates\":[%.7f,%.7f]}", e.Lon, e.Lat))
-			// 	// 属性文字のエスケープ関連文字の訂正
-			// 	if strings.Contains(e.Tags.Find("name"), "\\") {
-			// 		file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\\", "", -1)))
-			// 	} else if strings.Contains(e.Tags.Find("name"), "\n") {
-			// 		file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\n", "", -1)))
-			// 	} else if strings.Contains(e.Tags.Find("name"), "\"") {
-			// 		file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\"", "　", -1)))
-			// 	} else {
-			// 		file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", e.Tags.Find("name")))
-			// 	}
-			// }
-			// nodes++
+		switch re := scanner.Object().(type) {
 		case *osm.Way:
 			// set way coordinates
-			if _, flg := mrway[int(e.ID)]; flg {
+			if _, flg := mrway[int(re.ID)]; flg {
 				//fmt.Println("Way:", int(e.ID))
 				var flon, flat, llon, llat float64
-				for i, v := range e.Nodes {
+				for i, v := range re.Nodes {
 					if i == 0 {
-						mrway[int(e.ID)] += fmt.Sprintf("[%.7f,%.7f]", v.Lon, v.Lat)
+						mrway[int(re.ID)] += fmt.Sprintf("[%.7f,%.7f]", v.Lon, v.Lat)
 						flon, flat = v.Lon, v.Lat
 					} else {
-						mrway[int(e.ID)] += fmt.Sprintf(",[%.7f,%.7f]", v.Lon, v.Lat)
+						mrway[int(re.ID)] += fmt.Sprintf(",[%.7f,%.7f]", v.Lon, v.Lat)
 						llon, llat = v.Lon, v.Lat
 					}
 				}
 				if llon == flon && llat == flat {
-					mrway[int(e.ID)] += "/close"
+					mrway[int(re.ID)] += "/close"
 				} else {
-					mrway[int(e.ID)] += "/open"
+					mrway[int(re.ID)] += "/open"
 				}
 			}
-			// if e.Tags.Find(tagname) == tagval {
-			// 	sways++
-			// 	file.WriteString(",\n")
-			// 	// 要素情報の出力
-			// 	if e.Polygon() {
-			// 		file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"Polygon\",")
-			// 		file.WriteString("\"coordinates\":[[")
-			// 	} else {
-			// 		file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"LineString\",")
-			// 		file.WriteString("\"coordinates\":[")
-			// 	}
+		}
+	}
+	scanner.Close()
 
-			// 	for i, v := range e.Nodes {
-			// 		if i > 0 {
-			// 			file.WriteString(",")
-			// 		}
-			// 		file.WriteString(fmt.Sprintf("[%.7f,%.7f]", v.Lon, v.Lat))
-			// 	}
-			// 	if e.Polygon() {
-			// 		file.WriteString("]]}")
-			// 	} else {
-			// 		file.WriteString("]}")
-			// 	}
+	// ====================================
+	// 2.create GeoJSON file
+	// ====================================
+	file, err := os.Create(outfname)
+	if err != nil {
+		fmt.Printf("could not create file: %v", err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-			// 	// file.WriteString(fmt.Sprintf("\"coordinates\":[%.7f,%.7f]}", e.Lon, e.Lat))
-			// 	// 属性文字のエスケープ関連文字の訂正
-			// 	if strings.Contains(e.Tags.Find("name"), "\\") {
-			// 		file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\\", "", -1)))
-			// 	} else if strings.Contains(e.Tags.Find("name"), "\n") {
-			// 		file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\n", "", -1)))
-			// 	} else if strings.Contains(e.Tags.Find("name"), "\"") {
-			// 		file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", strings.Replace(e.Tags.Find("name"), "\"", "　", -1)))
-			// 	} else {
-			// 		file.WriteString(fmt.Sprintf(",\"properties\":{\"name\":\"%s\"}}", e.Tags.Find("name")))
-			// 	}
-			// }
-			ways++
+	// for debug
+	nodes, ways, relations := 0, 0, 0
+	snodes, sways, srelations := 0, 0, 0
+
+	// ====================================
+	// 3.recreate scanner and write GeoJSON
+	// ====================================
+	f.Seek(0, 0)
+	scanner = osmpbf.New(context.Background(), f, cpu)
+	scanner.SkipNodes = true
+	scanner.SkipWays = true
+
+	// write "FeatureCollection" record
+	file.WriteString("{\"type\":\"FeatureCollection\",\"features\":[\n")
+
+	bfirst, bmp := true, true
+	for scanner.Scan() {
+		switch e := scanner.Object().(type) {
 		case *osm.Relation:
 			if e.Tags.Find(tagname) == tagval {
 				srelations++
-				file.WriteString(",\n")
+				if bfirst {
+					bfirst = false
+				} else {
+					file.WriteString(",\n")
+				}
 
 				// 要素情報の出力
 				switch e.Tags.Find("type") {
@@ -178,12 +138,16 @@ func main() {
 					// continue
 					file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"MultiPolygon\",")
 					file.WriteString("\"coordinates\":[")
+					bmp = true
 				case "site":
-					continue
-					// file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"LineString\",")
-					// file.WriteString("\"coordinates\":[")
+					file.WriteString("{\"type\":\"Feature\",\"geometry\":{\"type\":\"MultiLineString\",")
+					file.WriteString("\"coordinates\":[")
+					bmp = false
 				}
 
+				// if e.Tags.Find("name") == "県立廿日市高等学校" {
+				// 	fmt.Println("")
+				// }
 				i := 0
 				lopen := false
 				for _, v := range e.Members {
@@ -209,13 +173,19 @@ func main() {
 							if wayelm[3] == "close" {
 								file.WriteString("[" + wayelm[2] + "]")
 							} else {
-								file.WriteString(wayelm[2])
-								lopen = true
+								if lopen {
+									file.WriteString(wayelm[2])
+								} else if i == 0 {
+									file.WriteString(wayelm[2])
+								} else {
+									file.WriteString("[" + wayelm[2])
+									lopen = true
+								}
 							}
 						}
 					}
 				}
-				if lopen {
+				if lopen && bmp {
 					file.WriteString("]]]}")
 				} else {
 					file.WriteString("]]}")
@@ -239,6 +209,7 @@ func main() {
 		}
 
 	}
+	scanner.Close()
 	// FeatureCollection終端を出力
 	file.WriteString("]}\n")
 
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module main
 
 go 1.16
 
-require (
-	github.com/paulmach/osm v0.2.2 // indirect
-	github.com/qedus/osmpbf v1.2.0
-)
+require github.com/paulmach/osm v0.2.2 // indirect
